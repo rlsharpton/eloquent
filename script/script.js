0d3a5d0ae4f2ab0f4e3f85afb86c72a4cee6726d
Robin Sharpton <robin@robinls.net>
2021-03-27 12:58:06 -0500

    stopping pyramid back to basics

--- script/script.js
+++ script/script.js
@@ -1,6 +1,7 @@
 
 const btn = document.querySelector('button');
 const canvas = document.querySelector('canvas');
+const paragraph = document.querySelector('p');
 
 let WIDTH = document.documentElement.clientWidth;
 let HEIGHT = document.documentElement.clientHeight;
@@ -8,19 +9,18 @@ let HEIGHT = document.documentElement.clientHeight;
 canvas.width = WIDTH;
 canvas.height = HEIGHT;
 
-function pyramid() {
+function pyramid(i) {
+  for(n = 0; n < i;n++) {
   console.log('#');
+  console.log(`I is: ${i} N is: ${n}`);
+  document.querySelector('P')
+  }
 }
 
 function draw() {
-n = 0;
-for(i = 1; i < 8; i++){
-  do {
-    pyramid();
-    console.log(`I is: ${i} and N is: ${n}`);
-    n++;
-  } while(n < i)
-}
+  for(i = 0; i < 8; i++){
+    pyramid(i);
+  }
 }
 
 btn.addEventListener('click', draw);
